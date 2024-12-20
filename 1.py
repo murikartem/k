cf001e4286f6c3b0713944e9f45d113e4114293b
kola899 <kola899@outlook.com>
2024-12-20 19:48:56 +0300

    ывфвыф

--- 1.py
+++ 1.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect
+from cachelib import FileSystemCache
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 import sqlite3
-
+from flask_session import Session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = '1234'
+app.config['SESSION_TYPE'] = 'cachelib'
+app.config['SESSION_CACHELIB'] = FileSystemCache(cache_dir='flask_session', threshold=500)
+Session(app)
+
+
 
 con = sqlite3.connect('datab.db', check_same_thread=False)
 cursor = con.cursor()
@@ -10,6 +18,9 @@ cursor = con.cursor()
 
 @app.route('/add/', methods=['POST', 'GET'])
 def add_page():
+    if 'login' not in session:
+        flash('Необходимо авторизоваться', 'danger')
+        return redirect(url_for('page_login'))
     return render_template('1.html')
 
 @app.route('/upload/', methods=['POST'])
@@ -39,10 +50,23 @@ def login_page():
 
 @app.route('/authorization/', methods=['POST', 'GET'])
 def authorization_page():
-    username = request.form['username']
+    login = request.form['username']
     password = request.form['password']
-    cursor.execute('SELECT username,passwords FROM users')
-    return redirect(url_for('login_page'))
+    cursor.execute('SELECT username, password FROM users')
+    data = cursor.fetchall()
+    print(login)
+    print(data)
+    if login in data and password in data:
+        flash('Авторизация прошла успешно', 'success')
+        session['login'] = True
+        session['username'] = login
+        session.permanent = False
+        app.permanent_session_lifetime = timedelta(minutes=1)
+        session.modified = True
+        return redirect(url_for('main_page'))
+    else:
+        flash('Авторизация прошла неуспешно', 'danger')
+        return redirect(url_for('login_page'))
 
 @app.route('/register/', methods=['POST', 'GET'])
 def register_page():
@@ -60,6 +84,13 @@ def save_page():
         patronymic = request.form['patronymic']
         cursor.execute('insert into users (name, last_name, gender, email, username, password, patronymic) values (?, ?, ?, ? ,? , ?, ?)', (name, last_name, gender, email, username, password, patronymic))
         con.commit()
+        flash('Регистрация прошла успешно','success')
         return redirect(url_for('login_page'))
+
+@app.route('/logout/')
+def logout():
+    session.clear()
+    flash('Вы вышли из профиля', 'danger')
+    return redirect(url_for('main_page'))
 
 app.run(debug=True)
